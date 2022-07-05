e1ee7fc191999922d9e50f2e362c28209b954e64
Zukisani Zamela <zukisani@terrafirma-software.com>
2022-07-05 13:55:47 +0200

    Completed 8.2.4 Testing layout changes

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:zuks)
+    @user_pw = "password"
+  end
   
   test "flash message does not persist" do
     get login_path
@@ -9,6 +14,35 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
       session: {
         email: "email@valid",
         password: "foo"
+      }
+    }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
+  test "login with valid info" do
+    get login_path
+    post login_path, params: { session: {
+        email: @user.email,
+        password: @user_pw
+      }
+    }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
+  test "login with valid email/invalid password" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: {
+        email: @user.email,
+        password: "invalid"
       }
     }
     assert_template 'sessions/new'
