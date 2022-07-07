8de3b6a3585a085ad3050425472448d8653f0857
Zukisani Zamela <zukisani@terrafirma-software.com>
2022-07-07 10:58:01 +0200

    Completed 10.1.4 Successful edits (with TDD)

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -20,4 +20,20 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     # It is just to demonstrate another way (possibly better) of testing for the right number of errors
     assert_select 'div#error_explanation ul li', count: 4
   end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
