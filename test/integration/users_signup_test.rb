244896ec89a9da0c95d5010d685b14fac691f568
Zukisani Zamela <zukisani@terrafirma-software.com>
2022-07-08 19:55:47 +0200

    Completed 11.2 Account activation emails

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -38,8 +38,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
     follow_redirect!
-    assert_template 'users/show'
+    assert_template 'static_pages/home'
     assert_not flash.empty?
-    assert is_logged_in?
   end
 end
