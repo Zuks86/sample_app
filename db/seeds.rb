569a51b181b28ef340545d4512b63e9c0110a8f4
Zukisani Zamela <zukisani@terrafirma-software.com>
2022-07-08 17:19:36 +0200

    Completed 11.1 Account activations resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@ User.create!(name: "Zukisani Zamela",
               email: "codewithzuks@gmail.com",
               password: "abc123",
               password_confirmation: "abc123",
-              admin: true)
+              admin: true,
+              activated: true,
+              activated_at: Time.zone.now)
 
 # Generate a bunch of additional users.
 99.times do |n|
@@ -14,5 +16,7 @@ User.create!(name: "Zukisani Zamela",
   User.create!(name: name,
                 email: email,
                 password: password,
-                password_confirmation: password)
+                password_confirmation: password,
+                activated: true,
+                activated_at: Time.zone.now)
 end
