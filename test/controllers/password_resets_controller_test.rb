923890aff6f594d21dea46c28889adfd5d4aa365
Zukisani Zamela <zukisani@terrafirma-software.com>
2022-07-10 16:27:45 +0200

    Completed 12.3 Resetting the password

--- test/controllers/password_resets_controller_test.rb
+++ test/controllers/password_resets_controller_test.rb
@@ -1,13 +1,7 @@
 require "test_helper"
 
 class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get password_resets_new_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get password_resets_edit_url
-    assert_response :success
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
